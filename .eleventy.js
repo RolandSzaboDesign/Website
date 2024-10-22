235e4dd86548a9dba8b45d28208c465c013b2280
Roland Szabó <hi@rolandszabo.design>
2024-10-22 16:42:48 +0200

    v1.0.0
    Website is Live!

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("src/favicons");
+	eleventyConfig.addPassthroughCopy("src/images");
+	eleventyConfig.addPassthroughCopy("src/css");
+	eleventyConfig.addWatchTarget("src/css/style.css");
+	eleventyConfig.setQuietMode(true);
+
+	return {
+		dir: {
+			input: "src",
+			output: "public",
+		}
+	};
+};
