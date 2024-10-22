3a3fb561bffff44fbd4caadf4d0d3478bbea7dd6
Roland Szabó <hi@rolandszabo.design>
2024-10-22 16:55:40 +0200

    v1.0.1
    PageSpeed optimizations

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/favicons");
 	eleventyConfig.addPassthroughCopy("src/images");
+	eleventyConfig.addPassthroughCopy("src/webfonts");
 	eleventyConfig.addPassthroughCopy("src/css");
 	eleventyConfig.addWatchTarget("src/css/style.css");
 	eleventyConfig.setQuietMode(true);
